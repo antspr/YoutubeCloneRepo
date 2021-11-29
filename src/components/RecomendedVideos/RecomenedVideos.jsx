0ffdd079c23a6f0766f85c8dae943768baafafb8
AustinH1131 <athleticatxbox2@gmail.com>
2021-11-29 07:53:37 -0800

    Added Componenets and worked on RecomenededVideos

---
+++ src/components/RecomendedVideos/RecomenedVideos.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './RecomendedVideos.css';
+
+const RecomendedVideos = (prop) => {
+
+    const handleClick = (event,id,title) => {
+        event.preventDefault()
+        PaymentResponse.getVideoIdTitle(id,title)
+    }
+
+    return(
+        <div className="recommendedVideos">
+            <h1>Recomended Videos</h1>
+            <div className="allRelatedVideos">
+                {PaymentResponse.videos.filter(videos => !videos.id.videoId.includes(props.videoId)).map(videos=> (
+                    <span>
+                        <div class= "relatedVideo">
+                        <input type= "image" onClick={(event)=> handleClick(event, videos.id.videoId, videos.snippit.title)} src={videos.snippit.thumbnails.medium.url}
+                            width={videos.snippit.thumbnails.medium.width}
+                            height={videos.snippit.thumbnails.medium.height} />
+                            <div class="relatedVideoTitle">
+                                {videos.snippet.title}
+
+                            </div>
+                        </div>
+                    </span>
+                  ))}
+            </div>
+        </div>
+    );
+}
+           
+export default RecomendedVideos;
+
+
+
+
+
+
+
+
