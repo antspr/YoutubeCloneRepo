5f7473f180ebd9d0663ff05cec299de51d9a1c73
AustinH1131 <athleticatxbox2@gmail.com>
2021-11-29 07:16:31 -0800

    Comments work CSS and database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import reactDOM  from 'react-dom'
+import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 reactDOM.render(<App.jsx/>, document.getElementById('root'));
