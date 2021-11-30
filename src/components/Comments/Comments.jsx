b10e323eae353e7277a40f4726cfe6485d8684ca
AustinH1131 <athleticatxbox2@gmail.com>
2021-11-30 12:09:15 -0800

    Updated Syntax Errors

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,41 +1,28 @@
-import React, {useState} from 'react';
+import React from "react";
 import "./Comments.css"
-import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Comments = (props) => {
 
-<<<<<<< HEAD
-=======
-    const [comment, setComment] = useState(this.comment);
-    //need to write this into http://127.0.0.1:8000/comments/ --->>> commentText (Charfield150) on backend
-    //where does comment live in relation to video on backend
-    const postComment = () => {
-        setComment : await axios.put('http://127.0.0.1:8000/comments/')
-
-    }  
->>>>>>> 8af3fc32392ca35751c1641135e9b7dc5e8c2786
     return (
         <div className="commentSection">
+            <h1>Comments</h1>
             <div className="userComment">
                 <h1>Comments</h1>
-<<<<<<< HEAD
                     {props.commentDetails.map(comments => (
                         <div>
                             {comments.comment_content}
                         </div>
                     ))}
                 <form action="/html/tags/html_form_tag_action.cfm" method="post">
-=======
-                <form action="submit" method={submitComment}>
->>>>>>> 8af3fc32392ca35751c1641135e9b7dc5e8c2786
-                <div class="commentBox">
-                    <textarea name={comment} id="comment">
+                <div class="mb-3">
+                    <textarea name="comments" id="comments">
                     Your Comment Here
                     </textarea>
                 </div>
-                <button onClick={setComment} value="Submit" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat-right-text" viewBox="0 0 16 16">
-
+                <button type="submit" value="Submit" class="commentButton"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#fff" class="commentButton" viewBox="0 0 16 16">
+                <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
+                <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
                 </svg></button>
                 </form>
                 
